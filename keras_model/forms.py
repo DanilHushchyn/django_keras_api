f783ddbeafcefa3f793e41c3c04ca55a2b17db39
rachel-liu-0104 <rl@singularitysystems.com>
2020-01-24 10:14:11 -0500

    Add image to result page

--- keras_model/forms.py
+++ keras_model/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    title = forms.ImageField()
+    image = forms.ImageField()
--- keras_model/views.py
+++ keras_model/views.py
@@ -7,16 +7,24 @@ from keras_model.predict import predict
 def index(request):
     return render(request, "keras_model/index.html")
 
+def save_uploaded_image(f,name):
+    with open("static/media/"+ name, "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
 # Create your views here.
 def predict_Image(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            image=request.POST.get['image']
-            result = predict(image.url)
+            image=request.FILES['image']
+            save_uploaded_image(image, image._name)
 
-        resp = {'data': image, 'result': result}
-        return render(request, "keras_model/result.html", resp)
+            result = predict("static/media/"+ image._name)[0][1]
+
+
+            resp = {'data':image._name , 'result': result}
+            return render(request, "keras_model/result.html", resp)
 
     else:
         return render(request, "keras_model/index.html")
